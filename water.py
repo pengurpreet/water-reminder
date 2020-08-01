68dcf695de94cc8c743573d4b9a6d9904a90a1bd
pengurpreet <69000770+pengurpreet@users.noreply.github.com>
2020-08-01 20:59:49 +0530

    Add files via upload

---
+++ water.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import time
+from plyer import notification
+if __name__ == "__main__":
+    notification.notify(
+        title = "Please drink water",
+        message = "pee la jalde nal kanjra", 
+        app_icon = "E:\icon.ico",          
+        timeout = 10,)
